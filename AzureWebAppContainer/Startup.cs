41956962f5000808f5c1939baea674d9f5c1194a
Christian Sparre <christian@sparre.it>
2019-02-26 10:02:55 +0100

    Build it

---
+++ AzureWebAppContainer/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureWebAppContainer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var version = "00010101.001-local";
+
+                var path = Path.Combine(typeof(Startup).Assembly.Location, "version.txt");
+                if (File.Exists(path))
+                {
+                    version = File.ReadAllText(path).Trim();
+                }
+
+                await context.Response.WriteAsync($"Hello World! I'm version '{version}'");
+            });
+        }
+    }
+}
